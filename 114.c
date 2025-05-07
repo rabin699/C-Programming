c88b163b841042e9bf72ab9a94371a8a09b339d1
rabin699 <rabinhumagain444@gmail.com>
2025-05-07 10:46:15 +0545

    updated

--- 114.c
+++ 114.c
@@ -1,3 +1,39 @@
 /*
 6.WAP to sort n numbers in ascending order using DMA and function.
 */
+#include <stdio.h>
+#include <stdlib.h>
+void sort(int arr[], int n);
+void main()
+{
+    int n;
+    printf("Enter the size of array:");
+    scanf("%d", &n);
+    int *arr = (int *)calloc(n, sizeof(int));
+    printf("Enter the elements:");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    sort(arr, n);
+    printf("Elements in ascending order is :\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+}
+void sort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - 1 - i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
